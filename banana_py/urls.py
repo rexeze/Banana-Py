ac88f18c2152a17a73d7f041709dbdf597bef3a1
Eze Sunday Eze <mailsforeze@gmail.com>
2018-12-10 07:04:40 +0100

    Updated Urls to Django 2.x

--- banana_py/urls.py
+++ banana_py/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from banana_py.views import BananasCompleteView
 
 urlpatterns = [
-    path('bananas/ripe/$', BananasCompleteView.as_view(), name='bananas_ripe'),
+    path('bananas/ripe/', BananasCompleteView.as_view(), name='bananas_ripe'),
 
 ]
