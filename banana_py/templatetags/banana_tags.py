56b3321b64b8c47fb4788b353995cefb7ab957e0
Eze Sunday Eze <mailsforeze@gmail.com>
2018-12-10 13:20:35 +0100

    updated banana_tags.py file

--- banana_py/templatetags/banana_tags.py
+++ banana_py/templatetags/banana_tags.py
@@ -4,6 +4,7 @@ from banana_py import Bananas_OAuth
 
 register = template.Library()
 
-@register.simple_tag
+
+@register.assignment_tag
 def banana_auth_url(link_text):
     return u'<a href="%s">%s</a>' % (Bananas_OAuth().authorize_url(), link_text)
